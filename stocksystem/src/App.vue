4be2a66bd4743aa98f610d259ab3a4a3db839ec7
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-06-13 10:10:25 -0300

    Correção do projeto vue-stocksystem, atualização da versão vue.js e criação de  projeto vue-stocksystem com typescript.

--- stocksystem/src/App.vue
+++ stocksystem/src/App.vue
@@ -6,20 +6,20 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
-export default Vue.extend({
-  name: 'app',
+@Component({
   components: {
-    HelloWorld
-  }
-});
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
